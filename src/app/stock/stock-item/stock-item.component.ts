52c41da56813627709bda3009b7c41ab70572b51
jackzhoumine <jackzhoumine@163.com>
2020-09-29 03:19:49 +0800

    绑定事件

--- src/app/stock/stock-item/stock-item.component.ts
+++ src/app/stock/stock-item/stock-item.component.ts
@@ -1,8 +1,9 @@
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 /*
  * @Description: 股票组件
  * @Date: 2020-09-29 01:33:58 +0800
  * @Author: JackChouMine
- * @LastEditTime: 2020-09-29 02:24:49 +0800
+ * @LastEditTime: 2020-09-29 03:19:34 +0800
  * @LastEditors: JackChouMine
  */
 import { Component, OnInit } from '@angular/core';
@@ -17,6 +18,8 @@ export class StockItemComponent implements OnInit {
   public code: string;
   public price: number;
   public previousPrice: number;
+  public positiveChange: boolean;
+  public isFavorite: boolean;
   constructor() {
     console.log('hello constructor');
   }
@@ -27,6 +30,14 @@ export class StockItemComponent implements OnInit {
     this.code = 'TSC';
     this.price = 86;
     this.previousPrice = 80;
+    this.positiveChange = this.price >= this.previousPrice;
+    this.isFavorite = false;
   }
-
+  // tslint:disable-next-line: typedef
+  toggleFavorite(event, param) {
+    console.log('用户点击了', event);
+    console.log('函数参数', param);
+    this.isFavorite = !this.isFavorite;
+    this.price = this.price - 20;
+  }
 }
