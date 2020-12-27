7c5c994ac41db867f82cf49fa7dea0fb79b7a20f
jack <jackzhoumine@gmail.com>
2020-12-28 02:50:40 +0800

    格式调整

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@
  * @Description: 应用根模块
  * @Date: 2020-09-28 03:04:08 +0800
  * @Author: JackChouMine
- * @LastEditTime: 2020-12-28 01:47:01 +0800
+ * @LastEditTime: 2020-12-28 02:50:18 +0800
  * @LastEditors: JackChou
  */
 import { BrowserModule } from '@angular/platform-browser'
@@ -14,7 +14,7 @@ import { StockItemComponent } from './stock/stock-item/stock-item.component'
 import { InputOutputComponent } from './stock/input-output/input-output.component'
 import { CreateStockComponent } from './stock/create-stock/create-stock.component'
 import { FormsModule } from '@angular/forms'
-import { FormInputComponent } from './form-input/form-input.component';
+import { FormInputComponent } from './form-input/form-input.component'
 @NgModule({
 	declarations: [
 		AppComponent,
